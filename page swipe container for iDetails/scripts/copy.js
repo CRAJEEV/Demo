21ba04c4de3661a7c2018b8a2260c21c65ccb981
rajeev kumar <rajeev kumar>
2017-01-26 16:19:48 +0530

    container added for demo

---
+++ page swipe container for iDetails/scripts/copy.js
@@ -0,0 +1,12 @@
+LANG.add({
+// tab data
+    tab1: "Tab1",
+    tab2: "Tab2",
+    tab3: "Tab3",
+    tab4: "Tab4",
+// end of tab data
+    text: "MOA",
+    ref_text: "References",
+    si_text: "Safety information",
+    pm_text: "Product Monograph"
+});
